703873b4ce81d209e3412b8ec133dd9b16cb4490
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-01-26 15:46:07 +0900

    Page Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import logo from "../images/logo.svg";
+import { pageLinks } from "../data"; 
 
 function Navbar() {
 	return (
@@ -15,29 +16,15 @@ function Navbar() {
 					</button>
 				</div>
 				<ul className="nav-links" id="nav-links">
-					<li>
-						<a href="#home" className="nav-link">
-							home
-						</a>
-					</li>
-
-					<li>
-						<a href="#about" className="nav-link">
-							about
-						</a>
-					</li>
-
-					<li>
-						<a href="#services" className="nav-link">
-							services
-						</a>
-					</li>
-
-					<li>
-						<a href="#tours" className="nav-link">
-							tours
-						</a>
-					</li>
+					{pageLinks.map((link) => {
+						return (
+							<li key={link.id}>
+								<a href={link.href} className="nav-link">
+									{link.text}
+								</a>
+							</li>
+						);
+					})}
 				</ul>
 
 				<ul className="nav-icons">
