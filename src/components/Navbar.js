3d40c0b16d40ba682b831a2cb35c04ed4a768c6f
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-01-26 16:48:24 +0900

    Alternative Approach - Page Links Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import logo from "../images/logo.svg";
 import { pageLinks, socialLinks } from "../data";
+import PageLinks from "./PageLinks";
 
 function Navbar() {
 	return (
@@ -15,17 +16,7 @@ function Navbar() {
 						<i className="fas fa-bars"></i>
 					</button>
 				</div>
-				<ul className="nav-links" id="nav-links">
-					{pageLinks.map((link) => {
-						return (
-							<li key={link.id}>
-								<a href={link.href} className="nav-link">
-									{link.text}
-								</a>
-							</li>
-						);
-					})}
-				</ul>
+				<PageLinks parentClass="nav-links" itemClass="nav-link" />
 
 				<ul className="nav-icons">
 					{socialLinks.map((link) => {
