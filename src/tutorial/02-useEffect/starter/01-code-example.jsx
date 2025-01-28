dbb70170ff5ad34824d59b505c100ca37bf99f7f
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-01-29 04:20:05 +0900

    useEffect

--- src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react';
 
 const CodeExample = () => {
-  const [value, setValue] = useState(0);
+	const [value, setValue] = useState(0);
 
-  return (
-    <div>
-      <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
-        click me
-      </button>
-    </div>
-  );
+	const sayHello = () => {
+		console.log('hello there');
+	};
+
+	return (
+		<div>
+			<h1>value : {value}</h1>
+			<button className="btn" onClick={() => setValue(value + 1)}>
+				click me
+			</button>
+		</div>
+	);
 };
 export default CodeExample;
--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,40 @@
+import { useState, useEffect } from 'react';
+
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		const dataFetched = async () => {
+			try {
+				const res = await fetch(url);
+				const users = await res.json();
+				setUsers(users);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		dataFetched();
+	}, []);
+	return (
+		<section>
+			<h3>github users</h3>
+			<ul className="users">
+				{users.map(user => {
+					const { id, login, avatar_url, html_url } = user;
+					return (
+						<li key={id}>
+							<img src={avatar_url} alt={login} />
+							<div>
+								<h5>{login}</h5>
+								<a href={html_url}>profile</a>
+							</div>
+						</li>
+					);
+				})}
+			</ul>
+		</section>
+	);
 };
 export default FetchData;
