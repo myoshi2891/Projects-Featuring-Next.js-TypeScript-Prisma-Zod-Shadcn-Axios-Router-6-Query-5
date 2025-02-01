0453c447f46d293d6cce7349aae3ef30956aeba7
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-02-01 22:15:53 +0900

    Tours - Refetch

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,24 @@ const App = () => {
 			</main>
 		);
   }
+
+  if (tours.length === 0) {
+		return (
+			<main>
+				<div className="title">
+					<h2>no tours left</h2>
+					<button
+						type="button"
+						style={{ marginTop: "2rem" }}
+						className="btn"
+						onClick={fetchTours}
+					>
+						refresh
+					</button>
+				</div>
+			</main>
+		);
+  }
   return (
 		<main>
 			<Tours tours={tours} removeTour={removeTour} />
