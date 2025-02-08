b7ff8e1c6d71e11371ac98fe7f166cf5259f3fd9
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-02-08 21:54:29 +0900

    Menu - Display Categories

---
+++ Menu/src/Categories.jsx
@@ -0,0 +1,15 @@
+function Categories({ categories }) {
+	return (
+		<div className="btn-container">
+			{categories.map((category) => {
+				return (
+					<button key={category} type="button" className="btn">
+						{category}
+					</button>
+				);
+			})}
+		</div>
+	);
+}
+
+export default Categories;
