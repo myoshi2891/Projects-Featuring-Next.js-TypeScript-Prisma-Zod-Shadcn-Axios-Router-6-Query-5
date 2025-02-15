8461c2a0611b2e8aae34ebda4b86a9d7ca2009e4
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-02-15 19:58:22 +0900

    Grocery Bud - Single Item

--- GroceryBud/src/SingleItem.jsx
+++ GroceryBud/src/SingleItem.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
 
-function SingleItem() {
-  return <div>SingleItem</div>;
+function SingleItem({ item, removeItem }) {
+    const [isChecked, setIsChecked] = useState(item.completed);
+    return (
+        <div className="single-item">
+            <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={() => setIsChecked(!isChecked)}
+            />
+            <p
+                style={{
+                    textTransform: "capitalize",
+                    textDecoration: isChecked && "line-through",
+                }}
+            >
+                {item.name}
+            </p>
+            <button className="btn remove-btn" type="button">
+                delete
+            </button>
+        </div>
+    );
 }
 
 export default SingleItem;
