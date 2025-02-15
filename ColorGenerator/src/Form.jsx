a0178bc38cda6a5eacf09bdf24dea85d10abeaa6
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-02-15 17:27:42 +0900

    Color Generator - HTML Color Input

--- ColorGenerator/src/Form.jsx
+++ ColorGenerator/src/Form.jsx
@@ -11,12 +11,23 @@ function Form() {
             <h4>Color Generator</h4>
             <form className="color-form" onSubmit={handleSubmit}>
                 <input
+                    type="color"
+                    value={color}
+                    onChange={(e) => setColor(e.target.value)}
+                />
+                <input
                     type="text"
                     value={color}
                     onChange={(e) => setColor(e.target.value)}
                     placeholder="#f15025"
                 />
-                <button className="btn" type="submit" style={{ background: color }}>Submit</button>
+                <button
+                    className="btn"
+                    type="submit"
+                    style={{ background: color }}
+                >
+                    Submit
+                </button>
             </form>
         </section>
     );
