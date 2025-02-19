ee017e974ea446ab1587e8e46baf3902b20b8213
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-02-19 23:45:32 +0900

    Sidebar/Modal - Sidebar Component JSX

--- SidebarModal/src/Sidebar.jsx
+++ SidebarModal/src/Sidebar.jsx
@@ -1,6 +1,43 @@
 
+import { useGlobalContext } from "./context";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
+import logo from "./logo.svg";
 function Sidebar() {
-  return <div>Sidebar</div>;
+    const { isSidebarOpen, closeSidebar } = useGlobalContext();
+    return (
+        <aside className="sidebar">
+            <div className="sidebar-header">
+                <img src={logo} alt="coding addict" className="logo" />
+                <button className="close-btn" onClick={closeSidebar}>
+                    <FaTimes />
+                </button>
+            </div>
+            <ul className="links">
+                {links.map((link) => {
+                    const { id, url, text, icon } = link;
+                    return (
+                        <li key={id}>
+                            <a href={url}>
+                                {icon}
+                                {text}
+                            </a>
+                        </li>
+                    );
+                })}
+            </ul>
+            <ul className="social-links">
+                {social.map((link) => {
+                    const { id, url, icon } = link;
+                    return (
+                        <li key={id}>
+                            <a href={url}>{icon}</a>
+                        </li>
+                    );
+                })}
+            </ul>
+        </aside>
+    );
 }
 
 export default Sidebar;
