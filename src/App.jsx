bead0d1c83e3163ed8e89bd09f82a50c3c741209
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-02-19 22:11:17 +0900

    05-leverage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { Test } from "./tutorial/04-project-structure/starter/Example/test";
-import Example from "./tutorial/04-project-structure/starter/Example";
+import List from "./tutorial/05-leverage-javascript/starter/List";
 
 function App() {
 	return (
 		<div className="container">
-			<Example />
-			<Test />
+			<h2>Leverage Javascript</h2>
+			<List />
 		</div>
 	);
 }
