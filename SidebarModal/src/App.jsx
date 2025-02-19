63dc4114fe0d6967be2eca853c148ee5ef99559b
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-02-19 23:18:21 +0900

    Sidebar/Modal - Home Component

--- SidebarModal/src/App.jsx
+++ SidebarModal/src/App.jsx
@@ -3,10 +3,12 @@ import Home from "./Home";
 import Modal from "./Modal";
 
 const App = () => {
-  return <main>
-    <Home />
-    <Modal />
-    <Sidebar/>
-  </main>;
+  return (
+      <>
+          <Home />
+          <Modal />
+          <Sidebar />
+      </>
+  );
 };
 export default App;
--- SidebarModal/src/Home.jsx
+++ SidebarModal/src/Home.jsx
@@ -1,10 +1,19 @@
 import { useGlobalContext } from "./context";
-
+import { FaBars } from "react-icons/fa";
 function Home() {
-    const { openSidebar, openModal } = useGlobalContext()
+    const { openSidebar, openModal } = useGlobalContext();
     console.log(openSidebar);
-    
-  return <div>Home</div>;
+
+    return (
+        <main>
+            <button onClick={openSidebar} className="sidebar-toggle">
+                <FaBars />
+            </button>
+            <div onClick={openModal} className="button btn">
+                show modal
+            </div>
+        </main>
+    );
 }
 
 export default Home;
