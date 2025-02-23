55ab85379221ccc34987aa31b5f721497c9beca0
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-02-23 19:18:42 +0900

    Strapi - Hero

--- Strapi/src/NavBar.jsx
+++ Strapi/src/NavBar.jsx
@@ -2,12 +2,16 @@ import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "./context";
 function NavBar() {
 	const { openSidebar } = useGlobalContext();
-  return <div className="nav-center">
-    <h3 className="logo">strapi</h3>
-    <button className="toggle-btn" onClick={openSidebar}>
-      <FaBars />
-    </button>
-  </div>;
+	return (
+		<nav>
+			<div className="nav-center">
+				<h3 className="logo">strapi</h3>
+				<button className="toggle-btn" onClick={openSidebar}>
+					<FaBars />
+				</button>
+			</div>
+		</nav>
+	);
 }
 
 export default NavBar;
