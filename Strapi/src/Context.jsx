e4146081dc41cd66441f475cddbcba722de60ed2
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-02-23 20:20:14 +0900

    Strapi - PageId Logic

--- Strapi/src/Context.jsx
+++ Strapi/src/Context.jsx
@@ -4,7 +4,7 @@ const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
 	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
+	const [pageId, setPageId] = useState(null);
 	const openSidebar = () => {
 		setIsSidebarOpen(true);
 	};
@@ -15,7 +15,13 @@ export const AppProvider = ({ children }) => {
 
 	return (
 		<AppContext.Provider
-			value={{ isSidebarOpen, openSidebar, closeSidebar }}
+			value={{
+				isSidebarOpen,
+				openSidebar,
+				closeSidebar,
+				pageId,
+				setPageId,
+			}}
 		>
 			{children}
 		</AppContext.Provider>
--- Strapi/src/NavLinks.jsx
+++ Strapi/src/NavLinks.jsx
@@ -1,15 +1,25 @@
 
-import sublinks from './data'
+import { useGlobalContext } from "./context";
+import sublinks from "./data";
 function NavLinks() {
+	const { setPageId } = useGlobalContext();
 
-    return <div className='nav-links'>
-        {sublinks.map((item) => {
-            const { pageId, page } = item
-            return <button key={pageId} className='nav-link'>
-                {page}
-            </button>
-      })}
-  </div>;
+	return (
+		<div className="nav-links">
+			{sublinks.map((item) => {
+				const { pageId, page } = item;
+				return (
+					<button
+						key={pageId}
+						className="nav-link"
+						onMouseEnter={() => setPageId(pageId)}
+					>
+						{page}
+					</button>
+				);
+			})}
+		</div>
+	);
 }
 
 export default NavLinks;
