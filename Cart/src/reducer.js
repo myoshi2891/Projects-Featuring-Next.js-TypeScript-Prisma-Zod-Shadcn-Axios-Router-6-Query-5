226ae1a601da07da7618ea46cd548a2cbc740119
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-01 19:49:12 +0900

    Cart - Increase Amount

--- Cart/src/reducer.js
+++ Cart/src/reducer.js
@@ -18,6 +18,16 @@ const reducer = (state, action) => {
 		return { ...state, cart: newCart };
 	}
 
+	if (action.type === INCREASE) {
+		const newCart = new Map(state.cart);
+		const itemId = action.payload.id;
+		const item = newCart.get(itemId);
+		const newItem = { ...item, amount: item.amount + 1 };
+		newCart.set(itemId, newItem);
+		// newCart.set(action.payload.id, state.cart.get(action.payload.id) + 1);
+		return { ...state, cart: newCart };
+	}
+
 	throw new Error(`no matching action type : ${action.type}`);
 };
 
