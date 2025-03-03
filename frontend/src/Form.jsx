9af2858bb972f6fd00a11d7d0de466567a2c6c93
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-03 23:19:46 +0900

    Create Task - Initial Setup

--- frontend/src/Form.jsx
+++ frontend/src/Form.jsx
@@ -1,26 +1,33 @@
-import { useState } from 'react';
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import customFetch from "./utils";
 
 const Form = () => {
-  const [newItemName, setNewItemName] = useState('');
+	const [newItemName, setNewItemName] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <h4>task bud</h4>
-      <div className='form-control'>
-        <input
-          type='text '
-          className='form-input'
-          value={newItemName}
-          onChange={(event) => setNewItemName(event.target.value)}
-        />
-        <button type='submit' className='btn'>
-          add task
-        </button>
-      </div>
-    </form>
-  );
+	const { mutate: createTask, isLoading } = useMutation({
+		mutationFn: (taskTitle) => customFetch.post("/", { title: taskTitle }),
+	});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		createTask(newItemName);
+	};
+	return (
+		<form onSubmit={handleSubmit}>
+			<h4>task bud</h4>
+			<div className="form-control">
+				<input
+					type="text "
+					className="form-input"
+					value={newItemName}
+					onChange={(event) => setNewItemName(event.target.value)}
+				/>
+				<button type="submit" className="btn" disabled={isLoading}>
+					add task
+				</button>
+			</div>
+		</form>
+	);
 };
 export default Form;
