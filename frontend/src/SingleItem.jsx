d628b300bfff15f09edfcb862d514d2951b090f6
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-03 23:48:12 +0900

    Edit Task - Complete

--- frontend/src/SingleItem.jsx
+++ frontend/src/SingleItem.jsx
@@ -1,27 +1,42 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import customFetch from "./utils";
+
 const SingleItem = ({ item }) => {
-  return (
-    <div className='single-item'>
-      <input
-        type='checkbox'
-        checked={item.isDone}
-        onChange={() => console.log('edit task')}
-      />
-      <p
-        style={{
-          textTransform: 'capitalize',
-          textDecoration: item.isDone && 'line-through',
-        }}
-      >
-        {item.title}
-      </p>
-      <button
-        className='btn remove-btn'
-        type='button'
-        onClick={() => console.log('delete task')}
-      >
-        delete
-      </button>
-    </div>
-  );
+	const queryClient = useQueryClient();
+	const { mutate: editTask } = useMutation({
+		mutationFn: ({ taskId, isDone }) => {
+			return customFetch.patch(`/${taskId}`, { isDone });
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ["tasks"] });
+		},
+	});
+
+	return (
+		<div className="single-item">
+			<input
+				type="checkbox"
+				checked={item.isDone}
+				onChange={() =>
+					editTask({ taskId: item.id, isDone: !item.isDone })
+				}
+			/>
+			<p
+				style={{
+					textTransform: "capitalize",
+					textDecoration: item.isDone && "line-through",
+				}}
+			>
+				{item.title}
+			</p>
+			<button
+				className="btn remove-btn"
+				type="button"
+				onClick={() => console.log("delete task")}
+			>
+				delete
+			</button>
+		</div>
+	);
 };
 export default SingleItem;
