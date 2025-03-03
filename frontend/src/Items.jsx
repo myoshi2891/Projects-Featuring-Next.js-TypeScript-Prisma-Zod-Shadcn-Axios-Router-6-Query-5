277aeeae50548b3dcd953a42970121ab7695f670
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-03 22:49:01 +0900

    Render Data

--- frontend/src/Items.jsx
+++ frontend/src/Items.jsx
@@ -1,11 +1,28 @@
-import SingleItem from './SingleItem';
-const Items = ({ items }) => {
-  return (
-    <div className='items'>
-      {items.map((item) => {
-        return <SingleItem key={item.id} item={item} />;
-      })}
-    </div>
-  );
+import { useQuery } from "@tanstack/react-query";
+import SingleItem from "./SingleItem";
+import customFetch from "./utils";
+
+const Items = () => {
+	const { isLoading, data } = useQuery({
+		queryKey: ["tasks"],
+		queryFn: async () => {
+			const { data } = await customFetch.get("/");
+			return data;
+		},
+	});
+
+	if (isLoading) {
+		return <p style={{ marginTop: "1rem" }}>Loading...</p>;
+	}
+
+	console.log(data);
+
+	return (
+		<div className="items">
+			{data.taskList.map((item) => {
+				return <SingleItem key={item.id} item={item} />;
+			})}
+		</div>
+	);
 };
 export default Items;
