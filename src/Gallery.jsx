c87a569a092a7f7a3b5acdc7e8a86916b8638873
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-06 00:40:10 +0900

    UseQuery Fix

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -7,7 +7,7 @@ const url = `https://api.unsplash.com/search/photos?client_id=${API_KEY}`;
 function Gallery() {
   const { searchTerm } = useGlobalContext();
   const response = useQuery({
-		queryKey: ["images"],
+		queryKey: ["images", searchTerm],
 		queryFn: async () => {
 			const result = await axios.get(`${url}&query=${searchTerm}`);
 
