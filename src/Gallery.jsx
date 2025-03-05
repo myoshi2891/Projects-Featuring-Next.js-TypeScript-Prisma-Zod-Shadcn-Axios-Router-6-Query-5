be29a1b4504dc63db2303ac39e932b4a6424fbaf
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-05 22:10:18 +0900

    Setup Query

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,6 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
+const API_KEY = import.meta.env.VITE_API_KEY;
+const url = `https://api.unsplash.com/search/photos?client_id=${API_KEY}&query=office`;
 function Gallery() {
-  return <div>Gallery</div>;
+	const response = useQuery({
+		queryKey: ["images"],
+		queryFn: async () => {
+			const result = await axios.get(url);
+
+			return result.data;
+		},
+	});
+
+	console.log(response);
+
+	return <div>Gallery</div>;
 }
 
 export default Gallery;
