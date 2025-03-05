81a6be71c0c30bebf9ad912edf2ac845ab80ee8a
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-05 22:19:33 +0900

    Render Images

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
-const url = `https://api.unsplash.com/search/photos?client_id=${API_KEY}&query=office`;
+const url = `https://api.unsplash.com/search/photos?client_id=${API_KEY}&query=omg`;
 function Gallery() {
 	const response = useQuery({
 		queryKey: ["images"],
@@ -13,9 +13,46 @@ function Gallery() {
 		},
 	});
 
-	console.log(response);
+	if (response.isLoading) {
+		return (
+			<section className="image-container">
+				<h4>Loading...</h4>
+			</section>
+		);
+	}
+	if (response.isError) {
+		return (
+			<section className="image-container">
+				<h4>There was an Error...</h4>
+			</section>
+		);
+	}
 
-	return <div>Gallery</div>;
+	const results = response.data.results;
+
+	if (results.length < 1) {
+		return (
+			<section className="image-container">
+				<h4>No results found...</h4>
+			</section>
+		);
+	}
+
+	return (
+		<section className="image-container">
+			{results.map((item) => {
+				const url = item?.urls?.regular;
+				return (
+					<img
+						src={url}
+						alt={item.alt_description}
+						key={item.id}
+						className="img"
+					/>
+				);
+			})}
+		</section>
+	);
 }
 
 export default Gallery;
