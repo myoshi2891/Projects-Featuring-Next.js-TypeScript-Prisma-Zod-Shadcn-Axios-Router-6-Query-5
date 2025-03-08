c2fc1033c2f44d6321d424fb5783b642db669a1e
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-08 20:50:06 +0900

    Fetch Projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,5 +1,9 @@
-import "./fetchProjects";
+import { useFetchProjects } from "./fetchProjects";
+
 function Projects() {
+	const { loading, projects } = useFetchProjects();
+	console.log(loading, projects);
+
 	return <h2>Projects</h2>;
 }
 
