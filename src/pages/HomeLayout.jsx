93dc9a29429193d2a35b560bde472f155b02333f
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-11 00:25:45 +0900

    Global Loading and Context

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 function HomeLayout() {
+	const navigation = useNavigation();
+	console.log(navigation);
+	const isPageLoading = navigation.state === "loading";
+	const value = "some value";
+
 	return (
 		<>
 			<Navbar />
 			<section className="page">
-				<Outlet />
+				{isPageLoading ? (
+					<div>Loading...</div>
+				) : (
+					<Outlet context={{ value }} />
+				)}
 			</section>
 		</>
 	);
