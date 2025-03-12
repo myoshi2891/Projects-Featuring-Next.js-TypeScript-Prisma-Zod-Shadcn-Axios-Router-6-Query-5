6cf7ac5916fd5c116b084365fe5edcdc6077be37
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-12 23:33:38 +0900

    Newsletter Structure

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,6 +1,55 @@
 
 function Newsletter() {
-  return <h1>Newsletter</h1>;
+  return (
+		<form action="" className="form">
+			<h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+				our newsletter
+			</h4>
+			<div className="form-row">
+				<label htmlFor="name" className="form-label">
+					name
+				</label>
+				<input
+					type="text"
+					className="form-input"
+					name="name"
+					id="name"
+					defaultValue="john"
+				/>
+			</div>
+			<div className="form-row">
+				<label htmlFor="lastName" className="form-label">
+					last name
+				</label>
+				<input
+					type="text"
+					className="form-input"
+					name="lastName"
+					id="lastName"
+					defaultValue="smith"
+				/>
+			</div>
+			<div className="form-row">
+				<label htmlFor="email" className="form-label">
+					email
+				</label>
+				<input
+					type="text"
+					className="form-input"
+					name="email"
+					id="email"
+					defaultValue="test@example.com"
+				/>
+			</div>
+			<button
+				type="submit"
+				className="btn btn-block"
+				style={{ marginTop: "0.5rem" }}
+			>
+				submit
+			</button>
+		</form>
+  );
 }
 
 export default Newsletter;
