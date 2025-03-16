922b2115bdff6d62cc9166a7da6e9ed004f57999
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-16 15:15:39 +0900

    SearchForm - Complete

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,6 +1,6 @@
 import Wrapper from "../assets/wrappers/SearchForm";
 import { Form, useNavigation } from "react-router-dom";
-function SearchForm() {
+function SearchForm({ searchTerm }) {
 	const navigation = useNavigation();
 	const isSubmitting = navigation.state === "submitting";
 
@@ -11,7 +11,7 @@ function SearchForm() {
 					type="search"
 					name="search"
 					className="form-input"
-					defaultValue="vodka"
+					defaultValue={searchTerm}
 				/>
 				<button type="submit" className="btn" disabled={isSubmitting}>
 					{isSubmitting ? "searching" : "Search"}
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,8 +5,9 @@ import SearchForm from "../components/SearchForm";
 const cocktailSearchUrl =
 	"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
-export const loader = async () => {
-	const searchTerm = "a";
+export const loader = async ({ request }) => {
+	const url = new URL(request.url);
+	const searchTerm = url.searchParams.get("search") || "gin";
 	const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
 	return { drinks: response.data.drinks, searchTerm };
 };
@@ -17,7 +18,7 @@ function Landing() {
 
 	return (
 		<>
-			<SearchForm />
+			<SearchForm searchTerm={searchTerm} />
 			<CocktailList drinks={drinks} />
 		</>
 	);
