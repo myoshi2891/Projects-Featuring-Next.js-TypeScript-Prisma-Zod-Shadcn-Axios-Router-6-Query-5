bc397e3b58f3fe8629ba8751dc3fd2609054a6b6
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-16 15:58:57 +0900

    React Query - Landing Page Setup

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,16 +5,39 @@ import SearchForm from "../components/SearchForm";
 const cocktailSearchUrl =
 	"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
+import { useQuery } from "@tanstack/react-query";
+
+const searchCocktailsQuery = (searchTerm) => {
+	return {
+		queryKey: ["search", searchTerm || "all"],
+		queryFn: async () => {
+			searchTerm = searchTerm || "a";
+			const response = await axios.get(
+				`${cocktailSearchUrl}${searchTerm}`
+			);
+			return response.data.drinks;
+		},
+	};
+};
+
 export const loader = async ({ request }) => {
 	const url = new URL(request.url);
+
 	const searchTerm = url.searchParams.get("search") || "gin";
-	const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-	return { drinks: response.data.drinks, searchTerm };
+	// const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+	return {
+		// drinks: response.data.drinks,
+		searchTerm,
+	};
 };
 
 function Landing() {
-	const { drinks, searchTerm } = useLoaderData();
-	console.log(drinks);
+	const { searchTerm } = useLoaderData();
+	const { data: drinks, isLoading } = useQuery(
+		searchCocktailsQuery(searchTerm)
+	);
+
+	if (isLoading) return <h4>Loading...</h4>;
 
 	return (
 		<>
