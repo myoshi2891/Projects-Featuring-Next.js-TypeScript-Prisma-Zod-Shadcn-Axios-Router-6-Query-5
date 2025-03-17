7165611d8b719df65e168970c3a453778d93001d
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-17 22:41:47 +0900

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,28 @@
-import { NavLink } from "react-router-dom";
-import Wrapper from "../assets/wrappers/Navbar";
+import { links } from "../data";
 
 function Navbar() {
 	return (
-		<Wrapper>
-			<div className="nav-center">
-				<span className="logo">MixMaster</span>
-				<div className="nav-links">
-					<NavLink to="/" className="nav-link">
-						Home
-					</NavLink>
-					<NavLink to="/about" className="nav-link">
-						About
-					</NavLink>
-					<NavLink to="/newsletter" className="nav-link">
-						Newsletter
-					</NavLink>
+		<nav className="bg-emerald-100">
+			<div className="mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8">
+				<h2 className="text-3xl font-bold">
+					Web <span className="text-emerald-600">Dev</span>
+				</h2>
+				<div className="flex gap-x-3">
+					{links.map((link) => {
+						const { id, href, text } = link;
+						return (
+							<a
+								key={id}
+								href={href}
+								className="capitalize text-lg tracking-wide hover:text-emerald-600 duration-300"
+							>
+								{text}
+							</a>
+						);
+					})}
 				</div>
 			</div>
-		</Wrapper>
+		</nav>
 	);
 }
 
