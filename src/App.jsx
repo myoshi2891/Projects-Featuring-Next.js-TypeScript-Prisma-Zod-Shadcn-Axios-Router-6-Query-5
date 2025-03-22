ee235cea1b2dcad261f9bd446bffede676002c39
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-22 18:37:14 +0900

    Create Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
-
+import {
+	About,
+	Cart,
+	Checkout,
+	Error,
+	HomeLayout,
+	Landing,
+	Login,
+	Orders,
+	Products,
+	Register,
+	SingleProduct,
+} from "./pages";
 function App() {
-  return <div>App</div>;
+	return <div>App</div>;
 }
 
 export default App;
---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,6 @@
+
+function HomeLayout() {
+  return <h1 className="text-4xl">HomeLayout</h1>;
+}
+
+export default HomeLayout;
