52408ae823d1ee75a7472a33e800a3ab927187bd
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-23 15:11:50 +0900

    About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,27 @@
-
 function About() {
-  return <h1 className="text-4xl">About</h1>;
+	return (
+		<>
+			<div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+				<h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+					We love
+				</h1>
+				<div className="stats bg-primary shadow">
+					<div className="stat">
+						<div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+							comfy
+						</div>
+					</div>
+				</div>
+			</div>
+			<p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit
+				fuga tenetur maiores maxime laborum repudiandae, mollitia magni
+				cupiditate ipsum. Facere, accusantium consequuntur ratione sequi
+				possimus ab assumenda ad quidem, reiciendis veritatis nisi
+				magnam excepturi minima molestiae deserunt maxime eos neque.
+			</p>
+		</>
+	);
 }
 
 export default About;
