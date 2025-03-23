cd4dae7225ffa7ff432c2c07d9b842624796e3cf
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-23 13:36:12 +0900

    Submit Button

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,6 +1,26 @@
+import { useNavigation } from "react-router-dom";
 
-function SubmitBtn({text}) {
-    return <button type="submit" className="btn btn-primary">{text}</button>;
+function SubmitBtn({ text }) {
+	const navigation = useNavigation();
+
+	const isSubmitting = navigation.state === "submitting";
+
+	return (
+		<button
+			type="submit"
+			className="btn btn-primary btn-block"
+			disabled={isSubmitting}
+		>
+			{isSubmitting ? (
+				<>
+					<span className="loading loading-spinner"></span>
+					sending...
+				</>
+			) : (
+				text || "submit"
+			)}
+		</button>
+	);
 }
 
 export default SubmitBtn;
