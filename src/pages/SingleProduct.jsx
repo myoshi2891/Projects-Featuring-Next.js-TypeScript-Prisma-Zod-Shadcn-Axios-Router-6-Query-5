974eeb0af05f6663d11466e83bb942c9bc6c4d2f
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-23 16:39:47 +0900

    Render Single Product

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -14,7 +14,38 @@ function SingleProduct() {
 		product.attributes;
 	const dollarsAmount = formatPrice(price);
 
-	return <div></div>;
+  return (
+		<section>
+			<div className="text-md breadcrumbs">
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/products">Products</Link>
+					</li>
+				</ul>
+			</div>
+			{/* PRODUCT */}
+			<div className="mt-6 grid gap-y-8 md:grid-cols-2 md:gap-x-16">
+				{/* IMAGE */}
+				<img
+					src={image}
+					alt={title}
+					className="w-96 h-96 object-cover rounded-lg lg:w-full"
+				/>
+				{/* PRODUCT */}
+				<div>
+					<h1 className="capitalize text-3xl font-bold">{title}</h1>
+					<h4 className="text-xl text-neutral-content font-bold mt-2">
+						{company}
+					</h4>
+					<p className="mt-3 text-xl">{dollarsAmount}</p>
+					<p className="mt-6 leading-8">{description}</p>
+				</div>
+			</div>
+		</section>
+  );
 }
 
 export default SingleProduct;
