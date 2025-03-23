6682882fa9efb9b16647fb1ab55f3f1a668cfc6f
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-23 15:52:35 +0900

    Landing Loader

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,14 @@
 import { Hero } from "../components";
+import { customFetch } from "../utils";
+
+const url = "/products?featured=true";
+
+export const loader = async () => {
+	const response = await customFetch(url);
+	const products = response.data.data;
+	return { products };
+};
+
 function Landing() {
 	return (
 		<>
