042e3442f5efc0e93ce6ff437f6b61a258e6b869
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-26 00:17:48 +0900

    Pagination - Complete

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -3,12 +3,17 @@ import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
 function PaginationContainer() {
 	const { meta } = useLoaderData();
 	const { pageCount, page } = meta.pagination;
+
 	const pages = Array.from({ length: pageCount }, (_, index) => {
 		return index + 1;
 	});
+	const { search, pathname } = useLocation();
+	const navigate = useNavigate();
 
 	const handlePageChange = (pageNumber) => {
-		console.log(pageNumber);
+		const searchParams = new URLSearchParams(search);
+		searchParams.set("page", pageNumber);
+		navigate(`${pathname}?${searchParams.toString()}`);
 	};
 
 	if (pageCount < 2) return null;
@@ -18,11 +23,14 @@ function PaginationContainer() {
 				<button
 					className="btn btn-xs sm:btn-md join-item"
 					onClick={() => {
-						handlePageChange("prev");
+						let prevPage = page - 1;
+						if (prevPage < 1) prevPage = pageCount;
+						handlePageChange(prevPage);
 					}}
 				>
 					Prev
 				</button>
+
 				{pages.map((pageNumber) => {
 					return (
 						<button
@@ -41,7 +49,9 @@ function PaginationContainer() {
 				<button
 					className="btn btn-xs sm:btn-md join-item"
 					onClick={() => {
-						handlePageChange("next");
+						let nextPage = page + 1;
+						if (nextPage > pageCount) nextPage = 1;
+						handlePageChange(nextPage);
 					}}
 				>
 					Next
