5a2b3d42f6dac652e3343f11820a86c03f1cb780
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-26 22:42:58 +0900

    Redux Toolkit - Setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+import { ToastContainer } from "react-toastify";
+import { store } from "./store.js";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<Provider store={store}>
+			<App />
+			<ToastContainer position="top-center" />
+		</Provider>
+	</StrictMode>
+);
