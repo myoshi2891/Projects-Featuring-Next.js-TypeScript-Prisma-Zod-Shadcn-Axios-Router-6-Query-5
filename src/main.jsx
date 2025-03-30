024705315e9a92923afad27d757e278e4d6c137f
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-30 15:55:58 +0900

    Refactor and Local Storage

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import { ToastContainer } from "react-toastify";
+
 import { store } from "./store.js";
 import { Provider } from "react-redux";
 import ErrorBoundary from "./ErrorBoundary.jsx";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Provider store={store}>
 			<App />
 			<ErrorBoundary>
-				<ToastContainer position="top-center" />
+				{/* <ToastContainer position="top-center" /> */}
 			</ErrorBoundary>
 		</Provider>
 	</StrictMode>
