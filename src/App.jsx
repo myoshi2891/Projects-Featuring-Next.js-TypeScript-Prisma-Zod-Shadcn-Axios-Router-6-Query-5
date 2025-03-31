185b1d24e73da0d06938be34cabb1ddc9cee44fc
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-01 00:21:18 +0900

    Register User - Docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import {
-	About,
-	Cart,
-	Checkout,
-	Error,
-	HomeLayout,
-	Landing,
-	Login,
-	Orders,
-	Products,
-	Register,
-	SingleProduct,
+    About,
+    Cart,
+    Checkout,
+    Error,
+    HomeLayout,
+    Landing,
+    Login,
+    Orders,
+    Products,
+    Register,
+    SingleProduct,
 } from "./pages";
 
 import { ErrorElement } from "./components";
@@ -21,63 +21,65 @@ import { loader as landingLoader } from "./pages/Landing";
 import { loader as singleProductLoader } from "./pages/SingleProduct";
 import { loader as productsLoader } from "./pages/Products";
 // actions
+import { action as registerAction } from "./pages/Register";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <HomeLayout />,
-		errorElement: <Error />,
-		children: [
-			{
-				index: true,
-				element: <Landing />,
-				errorElement: <ErrorElement />,
-				loader: landingLoader,
-			},
-			{
-				path: "products",
-				element: <Products />,
-				errorElement: <ErrorElement />,
-				loader: productsLoader,
-			},
-			{
-				path: "products/:id",
-				element: <SingleProduct />,
-				errorElement: <ErrorElement />,
-				loader: singleProductLoader,
-			},
-			{
-				path: "cart",
-				element: <Cart />,
-			},
-			{
-				path: "about",
-				element: <About />,
-			},
-			{
-				path: "checkout",
-				element: <Checkout />,
-			},
-			{
-				path: "orders",
-				element: <Orders />,
-			},
-		],
-	},
-	{
-		path: "/login",
-		element: <Login />,
-		errorElement: <Error />,
-	},
-	{
-		path: "/register",
-		element: <Register />,
-		errorElement: <Error />,
-	},
+    {
+        path: "/",
+        element: <HomeLayout />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <Landing />,
+                errorElement: <ErrorElement />,
+                loader: landingLoader,
+            },
+            {
+                path: "products",
+                element: <Products />,
+                errorElement: <ErrorElement />,
+                loader: productsLoader,
+            },
+            {
+                path: "products/:id",
+                element: <SingleProduct />,
+                errorElement: <ErrorElement />,
+                loader: singleProductLoader,
+            },
+            {
+                path: "cart",
+                element: <Cart />,
+            },
+            {
+                path: "about",
+                element: <About />,
+            },
+            {
+                path: "checkout",
+                element: <Checkout />,
+            },
+            {
+                path: "orders",
+                element: <Orders />,
+            },
+        ],
+    },
+    {
+        path: "/login",
+        element: <Login />,
+        errorElement: <Error />,
+    },
+    {
+        path: "/register",
+        element: <Register />,
+        errorElement: <Error />,
+        action: registerAction,
+    },
 ]);
 
 function App() {
-	return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
