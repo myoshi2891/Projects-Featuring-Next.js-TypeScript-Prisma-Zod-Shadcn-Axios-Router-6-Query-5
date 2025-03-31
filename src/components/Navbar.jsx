a570e3a4633f51b23f89208b74fec51a3e669fee
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-31 23:43:44 +0900

    Refactor Theme Toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,95 +1,78 @@
 import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
 import { FaBarsStaggered } from "react-icons/fa6";
-import { Link, NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
 import NavLinks from "./NavLinks";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { toggleTheme } from "../features/user/userSlice";
 
-const themes = {
-	winter: "winter",
-	dracula: "dracula",
-};
-
-const getThemeFromLocalStorage = () => {
-	return localStorage.getItem("theme") || themes.winter;
-};
 function Navbar() {
-	const [theme, setTheme] = useState(getThemeFromLocalStorage());
+    const dispatch = useDispatch();
+    const handleTheme = () => {
+        dispatch(toggleTheme());
+    };
 
-	const handleTheme = () => {
-		const { winter, dracula } = themes;
-		const newTheme = theme === winter ? dracula : winter;
+    const numItemsInCart = useSelector(
+        (state) => state.cartState.numItemsInCart
+    );
 
-		setTheme(newTheme);
-	};
-
-	useEffect(() => {
-		document.documentElement.setAttribute("data-theme", theme);
-		localStorage.setItem("theme", theme);
-	}, [theme]);
-
-	const numItemsInCart = useSelector(
-		(state) => state.cartState.numItemsInCart
-	);
-
-	return (
-		<nav className="bg-base-200">
-			<div className="navbar align-element">
-				<div className="navbar-start">
-					{/* TITLE */}
-					<NavLink
-						to="/"
-						className="hidden md:flex btn btn-primary text-3xl items-center"
-					>
-						C
-					</NavLink>
-					{/* DROPDOWN */}
-					<div className="dropdown">
-						<label
-							htmlFor=""
-							tabIndex={0}
-							className="btn btn-ghost md:hidden"
-						>
-							<FaBarsStaggered className="h-6 w-6" />
-						</label>
-						<ul
-							tabIndex={0}
-							className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
-						>
-							<NavLinks />
-						</ul>
-					</div>
-				</div>
-				<div className="navbar-center hidden md:flex">
-					<ul className="menu menu-horizontal">
-						<NavLinks />
-					</ul>
-				</div>
-				<div className="navbar-end">
-					{/* THEME SETUP */}
-					<label className="swap swap-rotate">
-						<input type="checkbox" onChange={handleTheme} />
-						{/* sun icon */}
-						<BsSunFill className="swap-on h-4 w-4" />
-						{/* moon icon */}
-						<BsMoonFill className="swap-off h-4 w-4" />
-					</label>
-					{/* CART LINK */}
-					<NavLink
-						to="/cart"
-						className="btn btn-ghost btn-circle btn-md ml-4"
-					>
-						<div className="indicator">
-							<BsCart3 className="h-6 w-6" />
-							<span className="badge badge-sm badge-primary indicator-item">
-								{numItemsInCart}
-							</span>
-						</div>
-					</NavLink>
-				</div>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="bg-base-200">
+            <div className="navbar align-element">
+                <div className="navbar-start">
+                    {/* TITLE */}
+                    <NavLink
+                        to="/"
+                        className="hidden md:flex btn btn-primary text-3xl items-center"
+                    >
+                        C
+                    </NavLink>
+                    {/* DROPDOWN */}
+                    <div className="dropdown">
+                        <label
+                            htmlFor=""
+                            tabIndex={0}
+                            className="btn btn-ghost md:hidden"
+                        >
+                            <FaBarsStaggered className="h-6 w-6" />
+                        </label>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
+                        >
+                            <NavLinks />
+                        </ul>
+                    </div>
+                </div>
+                <div className="navbar-center hidden md:flex">
+                    <ul className="menu menu-horizontal">
+                        <NavLinks />
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    {/* THEME SETUP */}
+                    <label className="swap swap-rotate">
+                        <input type="checkbox" onChange={handleTheme} />
+                        {/* sun icon */}
+                        <BsSunFill className="swap-on h-4 w-4" />
+                        {/* moon icon */}
+                        <BsMoonFill className="swap-off h-4 w-4" />
+                    </label>
+                    {/* CART LINK */}
+                    <NavLink
+                        to="/cart"
+                        className="btn btn-ghost btn-circle btn-md ml-4"
+                    >
+                        <div className="indicator">
+                            <BsCart3 className="h-6 w-6" />
+                            <span className="badge badge-sm badge-primary indicator-item">
+                                {numItemsInCart}
+                            </span>
+                        </div>
+                    </NavLink>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
