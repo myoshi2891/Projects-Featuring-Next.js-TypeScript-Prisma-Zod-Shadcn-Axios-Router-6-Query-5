d86c934560d9832dfb6550810cbc5a50083334c7
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-01 00:06:19 +0900

    Logout and Access User

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,43 +3,42 @@ import { CartItemsList, CartTotals, SectionTitle } from "../components";
 import { Link } from "react-router-dom";
 
 function Cart() {
-	// temp
-	const user = null;
-	const numItemsInCart = useSelector(
-		(state) => state.cartState.numItemsInCart
-	);
+    const user = useSelector((state) => state.userState.user);
+    const numItemsInCart = useSelector(
+        (state) => state.cartState.numItemsInCart
+    );
 
-	if (numItemsInCart === 0) {
-		return <SectionTitle text="Your cart is empty" />;
-	}
-	return (
-		<>
-			<SectionTitle text="Shopping Cart" />
-			<div className="mt-8 grid gap-8 lg:grid-cols-12">
-				<div className="lg:col-span-8">
-					<CartItemsList />
-				</div>
-				<div className="lg:col-span-4 lg:pl-4">
-					<CartTotals />
-					{user ? (
-						<Link
-							to="/checkout"
-							className="btn btn-primary btn-block mt-8"
-						>
-							proceed to checkout
-						</Link>
-					) : (
-						<Link
-							to="/checkout"
-							className="btn btn-primary btn-block mt-8"
-						>
-							please login
-						</Link>
-					)}
-				</div>
-			</div>
-		</>
-	);
+    if (numItemsInCart === 0) {
+        return <SectionTitle text="Your cart is empty" />;
+    }
+    return (
+        <>
+            <SectionTitle text="Shopping Cart" />
+            <div className="mt-8 grid gap-8 lg:grid-cols-12">
+                <div className="lg:col-span-8">
+                    <CartItemsList />
+                </div>
+                <div className="lg:col-span-4 lg:pl-4">
+                    <CartTotals />
+                    {user ? (
+                        <Link
+                            to="/checkout"
+                            className="btn btn-primary btn-block mt-8"
+                        >
+                            proceed to checkout
+                        </Link>
+                    ) : (
+                        <Link
+                            to="/checkout"
+                            className="btn btn-primary btn-block mt-8"
+                        >
+                            please login
+                        </Link>
+                    )}
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Cart;
