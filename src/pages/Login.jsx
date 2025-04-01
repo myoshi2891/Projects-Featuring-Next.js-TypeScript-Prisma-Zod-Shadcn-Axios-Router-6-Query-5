da1b9811b028ecbd88f5dce04fa068aff136064c
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-01 22:19:11 +0900

    Demo User

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,6 +23,22 @@ export const action =
         }
     };
 function Login() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const loginAsGuestUser = async () => {
+        try {
+            const response = await customFetch.post("/auth/local", {
+                identifier: "test@test.com",
+                password: "secret",
+            });
+            dispatch(loginUser(response.data));
+            toast.success("Welcome guest user!");
+            navigate("/");
+        } catch (error) {
+            console.log(error);
+            toast.error("Failed to login as guest user");
+        }
+    };
     return (
         <section className="h-screen grid place-items-center">
             <Form
@@ -30,22 +46,16 @@ function Login() {
                 className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
             >
                 <h4 className="text-center text-3xl font-bold">Login</h4>
-                <FormInput
-                    type="email"
-                    label="email"
-                    name="identifier"
-                    defaultValue="test@test.com"
-                />
-                <FormInput
-                    type="password"
-                    label="password"
-                    name="password"
-                    defaultValue="secret"
-                />
+                <FormInput type="email" label="email" name="identifier" />
+                <FormInput type="password" label="password" name="password" />
                 <div className="mt-4">
                     <SubmitBtn text="login" />
                 </div>
-                <div type="button" className="btn btn-secondary btn-block">
+                <div
+                    type="button"
+                    className="btn btn-secondary btn-block"
+                    onClick={loginAsGuestUser}
+                >
                     guest user
                 </div>
                 <p className="text-center">
