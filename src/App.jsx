fad94b7c435c515b45702b6e017bab70b4688a87
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-04 00:49:32 +0900

    Checkout Form - Setup

--- src/App.jsx
+++ src/App.jsx
@@ -24,64 +24,66 @@ import { loader as checkoutLoader } from "./pages/Checkout";
 // actions
 import { action as registerAction } from "./pages/Register";
 import { action as loginAction } from "./pages/Login";
+import { action as checkoutAction } from "./components/CheckoutForm";
 
 import { store } from "./store";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomeLayout />,
-        errorElement: <Error />,
-        children: [
-            {
-                index: true,
-                element: <Landing />,
-                errorElement: <ErrorElement />,
-                loader: landingLoader,
-            },
-            {
-                path: "products",
-                element: <Products />,
-                errorElement: <ErrorElement />,
-                loader: productsLoader,
-            },
-            {
-                path: "products/:id",
-                element: <SingleProduct />,
-                errorElement: <ErrorElement />,
-                loader: singleProductLoader,
-            },
-            {
-                path: "cart",
-                element: <Cart />,
-            },
-            {
-                path: "about",
-                element: <About />,
-            },
-            {
-                path: "checkout",
-                element: <Checkout />,
-                loader: checkoutLoader(store),
-            },
-            {
-                path: "orders",
-                element: <Orders />,
-            },
-        ],
-    },
-    {
-        path: "/login",
-        element: <Login />,
-        errorElement: <Error />,
-        action: loginAction(store),
-    },
-    {
-        path: "/register",
-        element: <Register />,
-        errorElement: <Error />,
-        action: registerAction,
-    },
+	{
+		path: "/",
+		element: <HomeLayout />,
+		errorElement: <Error />,
+		children: [
+			{
+				index: true,
+				element: <Landing />,
+				errorElement: <ErrorElement />,
+				loader: landingLoader,
+			},
+			{
+				path: "products",
+				element: <Products />,
+				errorElement: <ErrorElement />,
+				loader: productsLoader,
+			},
+			{
+				path: "products/:id",
+				element: <SingleProduct />,
+				errorElement: <ErrorElement />,
+				loader: singleProductLoader,
+			},
+			{
+				path: "cart",
+				element: <Cart />,
+			},
+			{
+				path: "about",
+				element: <About />,
+			},
+			{
+				path: "checkout",
+				element: <Checkout />,
+				loader: checkoutLoader(store),
+				action: checkoutAction(store),
+			},
+			{
+				path: "orders",
+				element: <Orders />,
+			},
+		],
+	},
+	{
+		path: "/login",
+		element: <Login />,
+		errorElement: <Error />,
+		action: loginAction(store),
+	},
+	{
+		path: "/register",
+		element: <Register />,
+		errorElement: <Error />,
+		action: registerAction,
+	},
 ]);
 
 function App() {
--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,5 +1,25 @@
+import { Form, redirect } from "react-router-dom";
+import FormInput from "./FormInput";
+import SubmitBtn from "./SubmitBtn";
+
+export const action = (store) => async () => {
+	console.log(store);
+	return null;
+};
+
 function CheckoutForm() {
-    return <div></div>;
+	return (
+		<Form method="POST" className="flex flex-col gap-y-4">
+			<h4 className="font-medium text-xl capitalize">
+				shipping information
+			</h4>
+			<FormInput label="first name" name="name" type="text" />
+			<FormInput label="address" name="address" type="text" />
+			<div className="mt-4">
+				<SubmitBtn text="place your order" />
+			</div>
+		</Form>
+	);
 }
 
 export default CheckoutForm;
