954cdc3b8f644126d0b0a3b4f54e4490b5d6347c
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-04 01:41:17 +0900

    Checkout Form - Complete

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -26,7 +26,7 @@ const userSlice = createSlice({
     initialState,
     reducers: {
         loginUser: (state, action) => {
-            const user = { ...action.payload.user, toen: action.payload.jwt };
+            const user = { ...action.payload.user, token: action.payload.jwt };
             state.user = user;
             localStorage.setItem("user", JSON.stringify(user));
         },
