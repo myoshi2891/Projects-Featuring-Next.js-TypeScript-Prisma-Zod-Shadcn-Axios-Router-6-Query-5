c0dbc1b8de5ccb86b8b9d74342ddaa436fb3f8a1
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-05 17:06:49 +0900

    React Query - Landing Page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,8 +2,12 @@ import { Hero, FeaturedProducts } from "../components";
 import { customFetch } from "../utils";
 const url = "/products?featured=true";
 
+const featuredProductsQuery = {
+	queryKey: ["featuredProducts"],
+	queryFn: () => customFetch(url),
+};
 export const loader = (queryClient) => async () => {
-	const response = await customFetch(url);
+	const response = await queryClient.ensureQueryData(featuredProductsQuery);
 	const products = response.data.data;
 	return { products };
 };
