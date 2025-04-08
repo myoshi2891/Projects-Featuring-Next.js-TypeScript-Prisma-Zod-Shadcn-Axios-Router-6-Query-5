67471f25b1a7b23d632034d46c8fdc8ba433e290
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-09 00:34:03 +0900

    Nested Layouts and Dynamic Pages

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const url = "https://www.course-api.com/react-tours-project";
 
 type Tour = {
@@ -15,29 +17,38 @@ const fetchTours = async () => {
 	return data;
 };
 async function ToursPage() {
-	const data = await fetchTours()
+	const data = await fetchTours();
 
 	return (
 		<section>
 			<h1 className="text-3xl mb-4">Tours</h1>
 			{data.map((tour: Tour) => (
-				<div key={tour.id} className="p-4 bg-white rounded shadow-md">
-					<img
-						src={tour.image}
-						alt={tour.name}
-						className="w-full h-64 object-cover"
-					/>
-					<h2 className="text-2xl font-bold">{tour.name}</h2>
-					<p className="text-gray-600">{tour.info}</p>
-					<div className="mt-4">
-						<span className="text-xl font-bold">
-							$ {tour.price}
-						</span>
-						<button className="ml-4 bg-blue-500 rounded text-white px-4 py-2">
-							Book Now
-						</button>
+				<>
+					<div
+						key={tour.id}
+						className="p-4 bg-white rounded shadow-md"
+					>
+						<img
+							src={tour.image}
+							alt={tour.name}
+							className="w-full h-64 object-cover"
+						/>
+						<Link key={tour.id} href={`/tours/${tour.id}`}>
+							<h2 className="text-2xl hover:text-blue-500 font-bold">
+								{tour.name}
+							</h2>
+							<p className="text-gray-600">{tour.info}</p>
+						</Link>
+						<div className="mt-4">
+							<span className="text-xl font-bold">
+								$ {tour.price}
+							</span>
+							<button className="ml-4 bg-blue-500 rounded text-white px-4 py-2">
+								Book Now
+							</button>
+						</div>
 					</div>
-				</div>
+				</>
 			))}
 		</section>
 	);
