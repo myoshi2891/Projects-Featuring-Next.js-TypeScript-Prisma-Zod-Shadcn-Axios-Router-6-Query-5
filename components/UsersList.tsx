afb5db5591e231485c8803e6be536ad88a60c7e8
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-11 14:48:05 +0900

    Delete Button, Hidden Input and Bind Method

--- components/UsersList.tsx
+++ components/UsersList.tsx
@@ -1,5 +1,5 @@
 import { fetchUsers } from "@/utils/actions";
-
+import DeleteButton from "./DeleteButton";
 async function UsersList() {
 	const users = await fetchUsers();
 	return (
@@ -8,8 +8,12 @@ async function UsersList() {
 				<div>
 					{users.map((user) => {
 						return (
-							<h4 key={user.id} className="capitalize text-lg">
+							<h4
+								key={user.id}
+								className="capitalize text-lg flex justify-between items-center mb-2"
+							>
 								{user.firstName} {user.lastName}
+								<DeleteButton id={user.id} />
 							</h4>
 						);
 					})}
