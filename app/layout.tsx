727fec068f4c0b676900139bc62290ac7ffea47e
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-11 20:05:56 +0900

    Navbar Component - Blueprint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Container from "@/components/global/Container";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,7 +17,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" suppressHydrationWarning>
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<Navbar />
+				<Container className="py-20">{children}</Container>
+			</body>
 		</html>
 	);
 }
