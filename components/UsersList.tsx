19d45f6e6e7a2efde03451ef82658417ae9ec030
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-11 17:15:31 +0900

    Middleware, Local Build and Cache

--- components/UsersList.tsx
+++ components/UsersList.tsx
@@ -5,7 +5,7 @@ async function UsersList() {
 	return (
 		<div className="mt-4">
 			{users.length ? (
-				<div>
+				<div className="max-w-lg">
 					{users.map((user) => {
 						return (
 							<h4
---
+++ middleware-copy.ts
@@ -0,0 +1,13 @@
+// import { NextResponse } from "next/server";
+
+// export function middleware(request: Request) { 
+
+//     console.log('Hello from middleware! Request URL:');
+//     // return Response.json({ msg: 'hello there' });
+//     return NextResponse.redirect(new URL('/', request.url))
+// }
+
+// export const config = {
+//     matcher: ['/about/:path*', '/tours/:path*']
+//     // matcher: '/about'
+// }
