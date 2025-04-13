abf4983477d57121b2b84ca2a2a53ed6f5b5079b
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-13 16:58:00 +0900

    Product Model

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,21 +1,5 @@
-import db from "@/utils/db";
-
-async function AboutPage() {
-	const profile = await db.testProfile.create({
-		data: {
-			name: "John Doe",
-		},
-	});
-
-	const users = await db.testProfile.findMany();
-
-	return (
-		<div>
-			{users.map((user) => {
-				return <p key={user.id}>{user.name}</p>;
-			})}
-		</div>
-	);
+function AboutPage() {
+	return <div>AboutPage</div>;
 }
 
 export default AboutPage;
