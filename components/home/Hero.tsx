6ff9bb55947350990ad8a76ae720b00b11eef7f0
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-15 00:28:42 +0900

    Hero Component

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,6 +1,27 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import HeroCarousel from "./HeroCarousel";
 
 function Hero() {
-  return <div>Hero</div>;
+	return (
+		<section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+			<div className="">
+				<h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+					We are changing the way people shop.
+				</h1>
+				<p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+					From the crowded streets of New York to the quaint cottages
+					of Vienna, there's a place where people can find what
+					they're looking for. Our online store is here to help you
+					find it.
+				</p>
+				<Button asChild size="lg" className="mt-10">
+					<Link href="/products">Our Products</Link>
+				</Button>
+			</div>
+			<HeroCarousel />
+		</section>
+	);
 }
 
 export default Hero;
