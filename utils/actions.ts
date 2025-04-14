482a38eae67196765f6575641108b08bba05c888
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-14 23:44:57 +0900

    Fetch Products Functions

---
+++ utils/actions.ts
@@ -0,0 +1,16 @@
+import db from "@/utils/db";
+
+export const fetchFeaturedProducts = async () => {
+	const products = await db.product.findMany({
+		where: { featured: true },
+	});
+	return products;
+};
+
+export const fetchAllProducts = async () => { 
+    return db.product.findMany({
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
+}
