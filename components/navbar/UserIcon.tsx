f4c55ef9ba72b86659589f9f4c119fdaf10a2e10
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-19 18:32:04 +0900

    UserIcon Component

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,25 @@
+import { LuUser } from "react-icons/lu";
+import {
+	currentUser,
+	// , auth
+} from "@clerk/nextjs/server";
+import Image from "next/image";
+async function UserIcon() {
+	// const { userId } = auth();
+	const user = await currentUser();
+	const profileImage = user?.imageUrl;
 
-function UserIcon() {
-  return <div>UserIcon</div>;
+	if (profileImage) {
+		return (
+			<Image
+				className="h-6 w-6 rounded-full object-cover"
+				src={profileImage}
+				alt="User"
+			/>
+		);
+	}
+
+	return <LuUser className="h-6 w-6 bg-primary rounded-full text-white" />;
 }
 
 export default UserIcon;
