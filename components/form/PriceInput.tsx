1ce7f02119247eba0a9cef7218a814ea52aeb67b
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-20 16:30:30 +0900

    Rest of Form Components

---
+++ components/form/PriceInput.tsx
@@ -0,0 +1,25 @@
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+
+const name = "price";
+type FormInputNumberProps = {
+	defaultValue?: number;
+};
+function PriceInput({ defaultValue = 0 }: FormInputNumberProps) {
+	return (
+		<div className="mb-2">
+			<Label htmlFor={name} className="capitalize">
+				Price ($)
+			</Label>
+			<Input
+				id={name}
+				type="number"
+				min={0}
+				defaultValue={defaultValue || 100}
+				required
+			/>
+		</div>
+	);
+}
+
+export default PriceInput;
