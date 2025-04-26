6331f1f909a5e5290ca4ce58de59215ecafd1660
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-26 13:34:03 +0900

    Favorites Page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import SectionTitle from "@/components/global/SectionTitle";
+import ProductsGrid from "@/components/products/ProductsGrid";
+import { fetchUserFavorites } from "@/utils/actions";
 
-function FavoritesPage() {
-  return <div>FavoritesPage</div>;
+async function FavoritesPage() {
+	const favorites = await fetchUserFavorites();
+	if (favorites.length === 0)
+		return <SectionTitle text="You have no favorites yet." />;
+
+	return (
+		<div>
+			<SectionTitle text="Your Favorites" />
+			<ProductsGrid
+				products={favorites.map((favorite) => favorite.product)}
+			/>
+		</div>
+	);
 }
 
 export default FavoritesPage;
