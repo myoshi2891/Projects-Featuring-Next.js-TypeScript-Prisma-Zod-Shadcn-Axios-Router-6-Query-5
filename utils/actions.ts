2a0bf8f892f6bd7aa2921468bb8618c2f5567ce6
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-26 16:29:39 +0900

    Product Reviews Component

--- utils/actions.ts
+++ utils/actions.ts
@@ -267,7 +267,17 @@ export const createReviewAction = async (
 	}
 };
 
-export const fetchProductReviews = async () => {};
+export const fetchProductReviews = async (productId: string) => {
+	const reviews = await db.review.findMany({
+		where: {
+			productId,
+		},
+		orderBy: {
+			createdAt: "desc",
+		},
+	});
+	return reviews;
+};
 export const fetchProductReviewsByUser = async () => {};
 export const deleteReviewAction = async () => {};
 export const findExistingReview = async () => {};
