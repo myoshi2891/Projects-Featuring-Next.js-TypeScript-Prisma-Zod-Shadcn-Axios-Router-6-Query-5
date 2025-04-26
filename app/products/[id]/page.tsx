c02c6ba3176a46153ce7fb46c3043538843261ea
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-26 14:03:47 +0900

    React Share Library

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,6 +5,7 @@ import { formatCurrency } from "@/utils/format";
 import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
 import AddToCart from "@/components/single-product/AddToCart";
 import ProductRating from "@/components/single-product/ProductRating";
+import ShareButton from "@/components/single-product/ShareButton";
 async function SingleProductPage({ params }: { params: { id: string } }) {
 	const product = await fetchSingleProduct(params.id);
 	const { name, image, company, description, price } = product;
@@ -31,18 +32,24 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
 						<h1 className="capitalize text-3xl font-bold">
 							{name}
 						</h1>
-						<FavoriteToggleButton productId={params.id} />
+						<div className="flex items-center gap-x-2">
+							<FavoriteToggleButton productId={params.id} />
+							<ShareButton
+								name={product.name}
+								productId={params.id}
+							/>
+						</div>
 					</div>
 					<ProductRating productId={params.id} />
 
 					<h4 className="text-xl mt-2">{company}</h4>
 					<p className="mt-3 text-md bg-muted inline-block p-2 rounded">
 						{dollarsAmount}
-                    </p>
-                    <p className="mt-6 leading-8 text-muted-foreground">
-                        {description}
-                    </p>
-                    <AddToCart productId={params.id} />
+					</p>
+					<p className="mt-6 leading-8 text-muted-foreground">
+						{description}
+					</p>
+					<AddToCart productId={params.id} />
 				</div>
 			</div>
 		</section>
