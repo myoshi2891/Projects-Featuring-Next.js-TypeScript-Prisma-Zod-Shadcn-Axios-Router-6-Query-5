bd0b8f43f7ff8f34ef0720972845857ffab18471
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-26 17:48:12 +0900

    Cart and CartItem Model

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import db from "@/utils/db";
-import { currentUser } from "@clerk/nextjs/server";
+import { auth, currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import {
 	imageSchema,
@@ -333,6 +333,7 @@ export const deleteReviewAction = async (prevState: { reviewId: string }) => {
 		return renderError(error);
 	}
 };
+
 export const findExistingReview = async (userId: string, productId: string) => {
 	return db.review.findFirst({
 		where: {
@@ -341,3 +342,31 @@ export const findExistingReview = async (userId: string, productId: string) => {
 		},
 	});
 };
+
+export const fetchCartItems = async () => {
+	const { userId } = auth();
+	const cart = await db.cart.findFirst({
+		where: {
+			clerkId: userId ?? "",
+		},
+		select: {
+			numItemsInCart: true,
+		},
+	});
+
+	return cart?.numItemsInCart || 0;
+};
+
+const fetchProduct = async () => {};
+
+export const fetchOrCreateCart = async () => {};
+
+const updateOrCreateCartItem = async () => {};
+
+export const updateCart = async () => {};
+
+export const addToCartAction = async () => {};
+
+export const removeCartItemAction = async () => {};
+
+export const updateCartItemAction = async () => {};
