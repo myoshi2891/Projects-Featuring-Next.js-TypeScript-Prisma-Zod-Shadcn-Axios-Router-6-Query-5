e5c0375de6ca2fbc7668a01af991c7b84874ff8d
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-27 12:05:34 +0900

    Cart Items List Component

--- components/cart/CartItemColumns.tsx
+++ components/cart/CartItemColumns.tsx
@@ -1,6 +1,55 @@
+import { formatCurrency } from "@/utils/format";
+import Image from "next/image";
+import Link from "next/link";
+
+export const FirstColumn = ({
+	name,
+	image,
+}: {
+	name: string;
+	image: string;
+}) => {
+	return (
+		<div className="relative h-24 w-24 sm:h-32 sm:w-32">
+			<Image
+				src={image}
+				alt={name}
+				fill
+				sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw"
+				priority
+				className="object-cover w-full rounded-md"
+			/>
+		</div>
+	);
+};
+
+export const SecondColumn = ({
+	name,
+	company,
+	productId,
+}: {
+	name: string;
+	company: string;
+	productId: string;
+}) => {
+	return (
+		<div className="sm:w-48">
+			<Link href={`/products/${productId}`}>
+				<h3 className="capitalize font-medium hover:underline">
+					{name}
+				</h3>
+			</Link>
+			<h4 className="mt-2 capitalize text-xs">{company}</h4>
+		</div>
+	);
+};
+
+export const FourthColumn = ({ price }: { price: number }) => {
+	return <p className="font-medium md:ml-auto">{formatCurrency(price)}</p>;
+};
 
 function CartItemColumns() {
-  return <div>CartItemColumns</div>;
+	return <div>CartItemColumns</div>;
 }
 
 export default CartItemColumns;
--- components/cart/CartItemsList.tsx
+++ components/cart/CartItemsList.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { Card } from "@/components/ui/card";
+import { FirstColumn, SecondColumn, FourthColumn } from "./CartItemColumns";
+import ThirdColumn from "./ThirdColumn";
+import { CartItemWithProduct } from "@/utils/types";
+function CartItemsList({ cartItems }: { cartItems: CartItemWithProduct[] }) {
+	return (
+		<div>
+			{cartItems.map((cartItem) => {
+				const { id, amount } = cartItem;
+				const {
+					image,
+					name,
+					company,
+					price,
+					id: productId,
+				} = cartItem.product;
 
-function CartItemsList() {
-  return <div>CartItemsList</div>;
+				return (
+					<Card
+						key={id}
+						className="flex flex-col gap-y-4 md:flex-row flex-wrap p-6 mb-8 gap-x-4"
+					>
+						<FirstColumn name={name} image={image} />
+						<SecondColumn
+							name={name}
+							company={company}
+							productId={productId}
+						/>
+						<ThirdColumn id={id} quantity={amount} />
+						<FourthColumn price={price} />
+					</Card>
+				);
+			})}
+		</div>
+	);
 }
 
 export default CartItemsList;
