3aed3dc3207512536f9a67a677a62d129c7a0b02
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-27 12:45:31 +0900

    Edit Cart Item Action

--- components/cart/ThirdColumn.tsx
+++ components/cart/ThirdColumn.tsx
@@ -5,17 +5,30 @@ import SelectProductAmount from "../single-product/SelectProductAmount";
 import { Mode } from "../single-product/SelectProductAmount";
 import FormContainer from "../form/FormContainer";
 import { SubmitButton } from "../form/Buttons";
-import { removeCartItemAction, updateCartItemAction } from "@/utils/actions";
+import {
+	removeCartItemAction,
+	updateCart,
+	updateCartItemAction,
+} from "@/utils/actions";
 // import { useToast } from "../ui/use-toast";
 import { useToast } from "../../hooks/use-toast";
 import { LargeNumberLike } from "crypto";
 
 function ThirdColumn({ quantity, id }: { quantity: number; id: string }) {
 	const [amount, setAmount] = useState(quantity);
+	const [isLoading, setIsLoading] = useState(false);
+	const { toast } = useToast();
 	const handleAmountChange = async (value: number) => {
+		setIsLoading(true);
+		toast({ description: `Calculating...` });
+		const result = await updateCartItemAction({
+			amount: value,
+			cartItemId: id,
+		});
 		setAmount(value);
+		toast({ description: result.message });
+		setIsLoading(false);
 	};
-	const toast = useToast();
 
 	return (
 		<div className="md:ml-8">
--- utils/actions.ts
+++ utils/actions.ts
@@ -525,7 +525,35 @@ export const removeCartItemAction = async (
 	}
 };
 
-export const updateCartItemAction = async () => {};
+export const updateCartItemAction = async ({
+	amount,
+	cartItemId,
+}: {
+	amount: number;
+	cartItemId: string;
+}) => {
+	const user = await getAuthUser();
+	try {
+		const cart = await fetchOrCreateCart({
+			userId: user.id,
+			errorOnFailure: true,
+		});
+		await db.cartItem.update({
+			where: {
+				id: cartItemId,
+				cartId: cart.id,
+			},
+			data: {
+				amount,
+			},
+		});
+		await updateCart(cart);
+		revalidatePath("/cart");
+		return { message: "Cart item updated successfully" };
+	} catch (error) {
+		return renderError(error);
+	}
+};
 
 export const createOrderAction = async (prevState: any, formData: FormData) => {
 	return { message: "order created successfully" };
